6d2c4540c87b87302f20165ada67c02f2050bad1
taktkk <takuyall717@gmail.com>
2022-11-14 15:00:30 +0900

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: () => import('../views/SigninView.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignupView.vue')
+    },
+    {
+      path: '/passwordreset',
+      name: 'passwordreset',
+      component: () => import('../views/PasswordResetView.vue')
+    },
+    {
+      path: '/passwordedit',
+      name: 'passwordedit',
+      component: () => import('../views/PasswordEditView.vue')
+    }
+  ]
+})
+
+export default router
