a7b1bec91dc4915a99254f0e4f93902e7fc86af4
taktkk <takuyall717@gmail.com>
2022-11-21 17:49:12 +0900

    layout 途中

--- src/views/MypageView.vue
+++ src/views/MypageView.vue
@@ -1,7 +1,59 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from "vue";
+
+const activeNames = ref(["1"]);
+const handleChange = (val: string[]) => {
+  console.log(val);
+};
 </script>
 
 <template>
-  
+  <el-row :gutter="20">
+    <el-col :offset="1" :span="6.5"><p style="background-color: #f4f4f5">決済がまだの場合はこちらで行ってください。</p></el-col>
+    <el-col :span="5" :offset="10"><el-button color="#B9A273" class="login-button">ユーザー編集</el-button></el-col>
+  </el-row>
+
+  <div class="demo-collapse">
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="2021/08/01【重要】新型コロナウイルス感染 予防対策への取組みについて" name="1">
+        <div>日頃より当店をご愛顧いただき、誠にありがとうございます。</div>
+        <div>
+          髪質改善専門ヘアサロン
+          varma（ヴェルマ）では、日頃からサロン内の衛生管理を徹底しておりますが、昨今の新型コロナウイルスの感染拡大に伴い、お客様の安心・安全のため、感染症予防対策や衛生管理をより一層強化しております。予防対策として下記の取り組みを実施していることをお知らせいたします。
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="2022/04/01【重要】新型コロナウイルス感染 予防対策への取組みについて" name="2">
+        <div>日頃より当店をご愛顧いただき、誠にありがとうございます。</div>
+        <div>
+          髪質改善専門ヘアサロン
+          varma（ヴェルマ）では、日頃からサロン内の衛生管理を徹底しておりますが、昨今の新型コロナウイルスの感染拡大に伴い、お客様の安心・安全のため、感染症予防対策や衛生管理をより一層強化しております。予防対策として下記の取り組みを実施していることをお知らせいたします。
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="2021/12/01【重要】新型コロナウイルス感染 予防対策への取組みについて" name="3">
+        <div>日頃より当店をご愛顧いただき、誠にありがとうございます。</div>
+        <div>
+          髪質改善専門ヘアサロン
+          varma（ヴェルマ）では、日頃からサロン内の衛生管理を徹底しておりますが、昨今の新型コロナウイルスの感染拡大に伴い、お客様の安心・安全のため、感染症予防対策や衛生管理をより一層強化しております。予防対策として下記の取り組みを実施していることをお知らせいたします。
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="2021/08/01【重要】新型コロナウイルス感染 予防対策への取組みについて" name="4">
+        <div>日頃より当店をご愛顧いただき、誠にありがとうございます。</div>
+        <div>
+          髪質改善専門ヘアサロン
+          varma（ヴェルマ）では、日頃からサロン内の衛生管理を徹底しておりますが、昨今の新型コロナウイルスの感染拡大に伴い、お客様の安心・安全のため、感染症予防対策や衛生管理をより一層強化しております。予防対策として下記の取り組みを実施していることをお知らせいたします。
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
 </template>
+
+<style scoped>
+.login-button {
+  color: white;
+}
+.demo-collapse {
+  width: 600px;
+  margin: 30px auto;
+}
+</style>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -7,6 +7,13 @@ import { CognitoUserPool, CognitoUser, AuthenticationDetails, CognitoUserAttribu
 // import Footer from '../components/Footer.vue'
 import { ref, reactive, computed } from "vue";
 
+const checkList = ref(["selected and disabled", "Option A"]);
+const radio1 = ref("1");
+const radio2 = ref("1");
+const radio3 = ref("1");
+const input1 = ref("");
+const input2 = ref("");
+
 const useremail = ref("");
 const password = ref("");
 const username = ref("");
@@ -58,24 +65,89 @@ const signup = () => {
 <template>
   <div class="signup">
     <h3>ユーザー情報</h3>
-    <el-form>
-      <div class="demo-input-size">
+    <el-row>
+      <el-col :span="9">
         <el-input type="email" class="email-form w-50 m-2" required v-model="useremail" placeholder="メールアドレス" />
+      </el-col>
+      <el-col :span="9">
         <el-input type="password" class="email-form w-50 m-2" required v-model="password" placeholder="パスワード" />
-      </div>
+      </el-col>
+    </el-row>
+    <el-row>
       <div class="demo-input-size">
         <el-input type="string" class="email-form w-50 m-2" required v-model="username" placeholder="氏名" />
         <el-input type="string" class="email-form w-50 m-2" required v-model="username_kana" placeholder="氏名（カタカナ）" />
       </div>
+    </el-row>
+    <el-row>
       <div class="demo-input-size">
         <el-input type="string" class="email-form w-50 m-2" required v-model="prefecture" placeholder="都道府県" />
         <el-input type="string" class="email-form w-60 m-2" required v-model="phonenumber" placeholder="電話番号" />
       </div>
+    </el-row>
+    <el-row>
       <div class="demo-input-size">
         <el-input type="string" class="email-form w-50 m-2" required v-model="birthday" placeholder="生年月日" />
       </div>
+    </el-row>
+
+    <el-row>
+      <p>質問事項</p>
+      <p>1. 髪や頭皮のお悩みに当てはまるモノにチェックをお願いします</p>
+      <el-checkbox-group v-model="checkList">
+        <el-checkbox label="くせ毛" />
+        <el-checkbox label="うねり" />
+        <el-checkbox label="白髪" />
+        <el-checkbox label="かゆみ" />
+        <el-checkbox label="フケ" />
+        <el-checkbox label="ハリコシ" />
+      </el-checkbox-group>
+    </el-row>
+    <div>2. お風呂上がりにドライヤーで髪や頭皮を乾かしていますか。</div>
+
+    <div class="flex">
+      <el-radio-group v-model="radio1">
+        <el-radio label="1" size="large">はい</el-radio>
+        <el-radio label="2" size="large">いいえ</el-radio>
+      </el-radio-group>
+    </div>
+
+    <p>3. 現在薬剤使用の美容メニューをしていますか</p>
+    <div class="flex">
+      <el-radio-group v-model="radio2">
+        <el-radio label="1" size="large">はい</el-radio>
+        <el-radio label="2" size="large">いいえ</el-radio>
+      </el-radio-group>
+    </div>
+    <p>4. 「３」ではいの方のみお答えください</p>
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox label="カラー" />
+      <el-checkbox label="パーマ" />
+      <el-checkbox label="ストレート" />
+      <el-checkbox label="その他" />
+    </el-checkbox-group>
+    <p>5. 今お使いのシャンプーやトリートメントを気に入っていますか</p>
+    <div class="flex">
+      <el-radio-group v-model="radio3">
+        <el-radio label="1" size="large">はい</el-radio>
+        <el-radio label="2" size="large">いいえ</el-radio>
+      </el-radio-group>
+    </div>
+    <p>6. 今回オンラインカウンセリングを受けてみようと思った理由をお教えください</p>
+    <el-row>
+      <el-col :span="12">
+        <el-input v-model="input1" placeholder="入力してください" />
+      </el-col>
+    </el-row>
+    <p>7. オンラインカウンセリングで期待する事をお教えください</p>
+    <el-row>
+      <el-col :span="12">
+        <el-input v-model="input2" placeholder="入力してください" />
+      </el-col>
+    </el-row>
+    <el-row>
       <el-button @click.prevent="signup" color="#B9A273" class="login-button">新規登録</el-button>
-    </el-form>
+    </el-row>
   </div>
 </template>
 
@@ -83,17 +155,17 @@ const signup = () => {
 .signup {
   margin: 0 auto;
   position: absolute;
-  width: 812px;
+  /* width: 812px; */
   height: 40px;
   left: 500px;
   top: 104px;
 }
 .login-button {
   color: white;
-  margin: 20px auto;
+  margin: 20px 150px;
 }
 .email-form {
-  margin: 20px auto;
+  margin: 10px 20px 10px 0;
 }
 
 .demo-input-size {
